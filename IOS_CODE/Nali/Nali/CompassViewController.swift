7bd39765575f51f8d8a94e23c813b9c48ade60ab
eknox <eknoxmobile@gmail.com>
2016-11-11 22:22:25 -0500

    Initial storyboard is done!

---
+++ IOS_CODE/Nali/Nali/CompassViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CompassViewController.swift
+//  Nali
+//
+//  Created by Evan Knox on 2016-11-11.
+//  Copyright © 2016 parchican. All rights reserved.
+//
+
+import UIKit
+
+class CompassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
