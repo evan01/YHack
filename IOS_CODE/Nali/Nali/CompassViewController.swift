f4ef7d1d281ca00ac3f40d85c20b40098c74cc5d
Jonathan Adalin <JonathanAdalin@gmail.com>
2016-11-12 11:50:18 -0500

    Fixed UI constraint issues

--- IOS_CODE/Nali/Nali/CompassViewController.swift
+++ IOS_CODE/Nali/Nali/CompassViewController.swift
@@ -14,6 +14,7 @@ import CoreLocation
 
 class CompassViewController: UIViewController, CLLocationManagerDelegate {
 
+
     @IBOutlet weak var arrow: UIImageView!
     
     let manager = CLLocationManager()
