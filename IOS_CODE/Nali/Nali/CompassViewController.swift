b128ae9472bc526b79b8893ca2eba6df2489c61f
Jonathan Adalin <JonAdalin@192.168.2.26>
2016-11-11 22:55:51 -0500

    Adding GPS basic functionality

--- IOS_CODE/Nali/Nali/CompassViewController.swift
+++ IOS_CODE/Nali/Nali/CompassViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
+import CoreLocation
 
-class CompassViewController: UIViewController {
 
+class CompassViewController: UIViewController, CLLocationManagerDelegate {
+
+    // map
+    @IBOutlet weak var map: MKMapView!
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        
+        print (location.coordinate.longitude)
+        print (location.coordinate.latitude)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
